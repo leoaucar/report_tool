c5daa1fec1739485d5a955277403a31d370a616f
Leonardo Aucar <leonardoaucar@gmail.com>
2018-08-20 20:31:22 -0300

    added coments to guide development

---
+++ report.py
@@ -0,0 +1,15 @@
+#responder:
+#1. Quais são os três artigos mais populares de todos os tempos?
+#2. Quem são os autores de artigos mais populares de todos os tempos?
+#3. Em quais dias mais de 1% das requisições resultaram em erros?
+
+#importar libraries necessárias
+
+
+#função de report
+#criar conexão com banco de dados
+#executar query de select
+#encerrar conexão
+
+#chamar a função para querys distintas
+#realizar print das querys
