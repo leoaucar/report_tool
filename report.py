870d73a1ce754c336bee9f9e8287a964700daa59
Leonardo Aucar <leonardoaucar@gmail.com>
2018-08-21 20:25:04 -0300

    added a basic query function to test connection to DB

--- report.py
+++ report.py
@@ -4,14 +4,29 @@
 #3. Em quais dias mais de 1% das requisições resultaram em erros?
 
 #importar libraries necessárias
+import psycopg2
 
 
 #função de report
+def query_maker(query_needed):
 #criar conexão com banco de dados
+    try:
+        conn = psycopg2.connect("newsdata")
+    except:
+        print ("Unable to connect to the database")
+    cur = conn.cursor()
 #executar query de select
+    cur.execute(query_needed)
+    result = cur.fetchall()
 #encerrar conexão
+    conn.close()
+#retorna resultados para serem usados
+    return result
 
 #chamar a função para querys distintas
+test_string = "SELECT * from authors LIMIT 5;"
+top_authors = query_maker(test_string)
 #realizar print das querys
+print(top_authors)
 
 #extrair os resultados para um arquivo.txt
