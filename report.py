db5fdf58c958bae47f16daa710ace14b49cd90d5
Leonardo Aucar <leonardoaucar@gmail.com>
2018-08-28 20:50:06 -0300

    reduced code duplicity by creating a final results variable

--- report.py
+++ report.py
@@ -17,7 +17,7 @@ def query_maker(query_needed):
 #busca os artigos mais populares
 #Faz um count da quantidade de logs associados à uma determinada slug
 #para isso realiza um Join
-top_articles_query ="SELECT count(*) as views, articles.title FROM log JOIN articles ON articles.slug = substring(log.path,10) GROUP BY articles.title ORDER BY views DESC;"
+top_articles_query ="SELECT count(articles.title) as views, articles.title FROM log JOIN articles ON articles.slug = substring(log.path,10) GROUP BY articles.title ORDER BY views DESC;"
 top_articles = query_maker(top_articles_query)
 
 #busca os autores mais populares
@@ -28,16 +28,18 @@ top_authors = query_maker(top_authors_query)
 top_errors_query = "SELECT * from log LIMIT 1;"
 top_errors = query_maker(top_errors_query)
 
-
-#realizar print das querys
-print("The most read articles are:\n" +
+final_result = ("The most read articles are:\n" +
 str(top_articles) + "\n\n" +
 "The top authors are:\n" +
 str(top_authors) + "\n\n" +
 "The days with more than 1% of errors are:\n" +
-str(top_errors) )
+str(top_errors))
+
+
+#realizar print das querys
+print(final_result)
 
 #extrair os resultados para um arquivo.txt
 #cria ou abre um arquivo txt e configura para sempre sobrescrever resultados pelos mais atuais
 results = open("results.txt","w")
-results.write(str(top_articles) + str(top_errors) + str(top_authors))
+results.write(final_result)
