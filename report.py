f1093e4d8fa9203a85d6a7c82c988e71e58a1008
Leonardo Aucar <leonardoaucar@gmail.com>
2018-09-05 20:01:35 -0300

    made adjustments to fit pycodestyle

--- report.py
+++ report.py
@@ -1,45 +1,66 @@
-#importar libraries necessárias
+#!/usr/bin/env python3
+
+# Importar libraries necessárias
 import psycopg2
 
 
-#função padrão para acessar o banco e executar uma query
+# Função padrão para acessar o banco e executar uma query
 def query_maker(query_needed):
     try:
         conn = psycopg2.connect("dbname=news")
     except:
-        print ("Unable to connect to the database")
+        print("Unable to connect to the database")
     cur = conn.cursor()
     cur.execute(query_needed)
     result = cur.fetchall()
     conn.close()
     return result
 
-#busca os artigos mais populares
-#Faz um count da quantidade de logs associados à uma determinada slug
-#para isso realiza um Join
-top_articles_query ="SELECT count(articles.title) as views, articles.title FROM log JOIN articles ON articles.slug = substring(log.path,10) GROUP BY articles.title ORDER BY views DESC;"
+
+# Busca os artigos mais populares
+# Faz um count da quantidade de logs associados à uma determinada slug
+# Para isso realiza um Join
+top_articles_query = ("SELECT count(articles.title) as views, " +
+                      "articles.title " +
+                      "FROM log JOIN articles " +
+                      "ON articles.slug = substring(log.path,10) " +
+                      "GROUP BY articles.title ORDER BY views DESC;")
 top_articles = query_maker(top_articles_query)
 
-#busca os autores mais populares
-top_authors_query = "SELECT count(authors.name) as views, authors.name FROM (articles JOIN log on articles.slug = substring(log.path,10)) JOIN authors ON articles.author = authors.id GROUP BY authors.name ORDER BY views DESC"
+# Busca os autores mais populares
+top_authors_query = ("SELECT count(authors.name) as views, authors.name " +
+                     "FROM (articles JOIN log " +
+                     "ON articles.slug = substring(log.path,10)) " +
+                     "JOIN authors ON articles.author = authors.id " +
+                     "GROUP BY authors.name ORDER BY views DESC;")
 top_authors = query_maker(top_authors_query)
 
-#busca dias com + de 1% de erros nas requests
-top_errors_query = "SELECT t1.day FROM (SELECT count(status) as oks, log.time::date as day FROM log WHERE status = '200 OK' GROUP BY day) as t1 LEFT JOIN (SELECT count(status) as wrongs, log.time::date as day FROM log WHERE status = '404 NOT FOUND' GROUP BY day) as t2 on t1.day = t2.day WHERE oks / 100 < wrongs;"
+# Busca dias com + de 1% de erros nas requests
+top_errors_query = ("SELECT t1.day FROM " +
+                    "(SELECT count(status) as oks, log.time::date as day " +
+                    "FROM log WHERE status = '200 OK' GROUP BY day) " +
+                    "as t1 LEFT JOIN " +
+                    "(SELECT count(status) as wrongs, " +
+                    "log.time::date as day " +
+                    "FROM log WHERE status = '404 NOT FOUND' " +
+                    "GROUP BY day) " +
+                    "as t2 on t1.day = t2.day " +
+                    "WHERE oks / 100 < wrongs;")
 top_errors = query_maker(top_errors_query)
 
 final_result = ("The most read articles are:\n" +
-str(top_articles) + "\n\n" +
-"The top authors are:\n" +
-str(top_authors) + "\n\n" +
-"The days with more than 1% of errors are:\n" +
-str(top_errors))
+                str(top_articles) + "\n\n" +
+                "The top authors are:\n" +
+                str(top_authors) + "\n\n" +
+                "The days with more than 1% of errors are:\n" +
+                str(top_errors))
 
 
-#realizar print das querys
+# Realizar print das querys
 print(final_result)
 
-#extrair os resultados para um arquivo.txt
-#cria ou abre um arquivo txt e configura para sempre sobrescrever resultados pelos mais atuais
-results = open("results.txt","w")
+# Extrair os resultados para um arquivo.txt
+# Cria ou abre um arquivo txt
+# Configura para sempre sobrescrever resultados pelos mais atuais
+results = open("results.txt", "w")
 results.write(final_result)
